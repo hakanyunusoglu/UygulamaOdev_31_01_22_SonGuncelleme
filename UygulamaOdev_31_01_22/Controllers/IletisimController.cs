24523bfcb820d2dd9687059345e4d1125f3b2da5
Hakan <Hakan@HAKANPC>
2022-01-31 22:31:36 +0300

    Add project files.

---
+++ UygulamaOdev_31_01_22/Controllers/IletisimController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UygulamaOdev_31_01_22.Controllers
+{
+    public class IletisimController : Controller
+    {
+        // GET: Iletisim
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+    }
+}
---
+++ UygulamaOdev_31_01_22/Controllers/TarihceController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UygulamaOdev_31_01_22.Models;
+using UygulamaOdev_31_01_22.Models.DTO;
+
+namespace UygulamaOdev_31_01_22.Controllers
+{
+    
+    public class TarihceController : Controller
+    {
+        // GET: Tarihce
+        sinavdbEntities4 db = new sinavdbEntities4();
+        ModelDB mdb = new ModelDB();
+        public ActionResult Tarihce()
+        {
+
+            List<TarihceDTO> tarihceList = db.Tarihces.Select(x => new TarihceDTO
+            {
+                Aciklama = x.Aciklama,
+                Baslik = x.Baslik,
+                Resim = x.Resim
+            }).ToList();
+            mdb.ozellikList = db.Ozelliks.Select(x => new OzellikDTO
+            { 
+              ButonRenk = x.ButonRenk,
+               Icon = x.Icon,
+                OzellikAciklama = x.Aciklama
+            }).ToList();
+            return View(tarihceList);
+        }
+    }
+}
